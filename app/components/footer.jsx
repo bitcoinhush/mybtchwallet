486640b0146f1b7e1a2b31bdc3eab2d09de5c7e5
miodragpop <37712022+miodragpop@users.noreply.github.com>
2019-03-12 11:19:31 +0100

    Update footer.jsx

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -17,14 +17,14 @@ export default class ZFooter extends React.Component {
   render () {
     return (
         <footer id="contact">
-		<div class="footer-bottom">
+		<div className="footer-bottom">
         <Container>
         <Row>
           <Col md="12">
-            <a href="#" data-toggle="modal" data-target="#faqopen" class="btn-white-line">FAQ</a>
-            <a href="#" data-toggle="modal" data-target="#guideopen" class="btn-white-line">Guide</a>
-            <p class="copyright m-3">MAKE SURE YOU ARE ON <b>WALLET.ZEROCLASSIC.ORG AND BOOKMARK IT</b></p>
-            <p class="copyright">Keys are validated client-side and do not leave your browser or network. You are responsible for keeping your own keys safe! SAVE YOUR PRIVATE KEY BEFORE SENDING OR RECEIVING, LEAVING THE PAGE WILL ERASE EVERYTHING.</p>
+            <a href="#" data-toggle="modal" data-target="#faqopen" className="btn-white-line">FAQ</a>
+            <a href="#" data-toggle="modal" data-target="#guideopen" className="btn-white-line">Guide</a>
+            <p className="copyright m-3">MAKE SURE YOU ARE ON <b>WALLET.ZEROCLASSIC.ORG AND BOOKMARK IT</b></p>
+            <p className="copyright">Keys are validated client-side and do not leave your browser or network. You are responsible for keeping your own keys safe! SAVE YOUR PRIVATE KEY BEFORE SENDING OR RECEIVING, LEAVING THE PAGE WILL ERASE EVERYTHING.</p>
           </Col>
         </Row>
         </Container>
