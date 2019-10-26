d7f8be1de0ad0e45e2e6bd2a30fb18cf8824a633
gilardh <gilardh@yahoo.com>
2019-10-27 00:00:30 +0200

    Update guide.js

--- app/guide.js
+++ app/guide.js
@@ -3,9 +3,9 @@ import 'react-table/react-table.css'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ZNavbar from './components/navbar.jsx'
-import ZGuide from './components/guide.jsx'
+//import ZGuide from './components/guide.jsx'
 import ZFooter from './components/footer.jsx'
 
 ReactDOM.render(<ZNavbar />, document.getElementById('navbar'));
-ReactDOM.render(<ZGuide />, document.getElementById('guide'));
+//ReactDOM.render(<ZGuide />, document.getElementById('guide'));
 ReactDOM.render(<ZFooter />, document.getElementById('footer'));
